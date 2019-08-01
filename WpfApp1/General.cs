7c13d1affeb474fb91484b48bcac9718313ef6c1
Melvin C Wiley <m_c_wiley@hotmail.com>
2019-08-01 09:10:08 -0400

    Add project files.

---
+++ WpfApp1/General.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    public static class General
+    {
+        public static string myDBConn = "Server=DESKTOP-SNIFN8M;Database=TestDB;Trusted_Connection=True;";
+
+        public static string Left(string param, int length)
+        {
+            string result = param.Substring(0, length);
+            return result;
+        }
+        public static string Right(string param, int length)
+        {
+            string result = param.Substring(param.Length - length, length);
+            return result;
+        }
+
+        public static string Mid(string param, int startIndex, int length)
+        {
+            string result = param.Substring(startIndex, length);
+            return result;
+        }
+
+        public static int Instr(string str, string srch, int strtpos)
+        {
+            int rtn = -1;
+            rtn = str.IndexOf(srch, strtpos);
+            return rtn;
+        }
+
+        public static DataTable GetData(string getSQL)
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(General.myDBConn);
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            command = new SqlCommand(getSQL, connection);
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+
+            return dt;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillDataGrid();
+        }
+
+        private void Clicker_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is the Clicker");
+        }
+
+        public void FillDataGrid()
+        {
+            dg_Topics.ItemsSource = General.GetData("select * from Topics").DefaultView;
+
+            myCombo.ItemsSource = General.GetData("select * from SubTopics").DefaultView;
+            myCombo.DisplayMemberPath = "SubTopicName";
+            myCombo.SelectedValuePath = "SubTopicID";
+        }
+
+        private void MyCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
