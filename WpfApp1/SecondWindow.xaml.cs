01f23b57966e0e046f6548a2e98adaf005d93988
Melvin C Wiley <m_c_wiley@hotmail.com>
2019-08-14 07:12:47 -0400

    Third Window commit

--- WpfApp1/SecondWindow.xaml.cs
+++ WpfApp1/SecondWindow.xaml.cs
@@ -11,11 +11,11 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +23,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
@@ -55,16 +56,40 @@ namespace WpfApp1
             this.Close();
         }
 
+        /// <summary>
+        /// Fills the listview.
+        /// </summary>
         private void FillListview()
         {
-            //lv_Contacts.ItemsSource = null;
-            //lv_Contacts.ItemsSource = General.GetDataTEST("select * from Topics").DefaultView;
             lv_Contacts.ItemsSource = General.GetDataTEST("select T.*, S.* from Topics T join SubTopics S on T.TopicID = S.TopicID").DefaultView;
-            //lv_Contacts.DisplayMemberPath = "SubTopicName";
-            //lv_Contacts.SelectedValuePath = "SubTopicID";
-
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            myBrowser.Navigate(new Uri("http://www.comcast.net"));
+        }
+        private void GoBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (myBrowser.CanGoBack)
+            {
+                myBrowser.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("Cannot Go back");
+            }
+        }
+        private void GoForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (myBrowser.CanGoForward)
+            {
+                myBrowser.GoForward();
+            }
+            else
+            {
+                MessageBox.Show("Cannot Go Forward");
+            }
+        }
 
     }
 }
